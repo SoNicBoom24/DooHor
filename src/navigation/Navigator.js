c26b1c29687aaeca007c8c36b674c78b14e80b8e
KITTIPOB PANGTRAKOON <63070010@kmitl.ac.th>
2022-10-31 18:58:33 +0700

    เพิ่มวิธีทำหน้าดีเทล

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -10,6 +10,7 @@ import { Ionicons } from '@expo/vector-icons';
 import ScreenHor from "../screens/SelectHor";
 import ScreenRegister from "../screens/Registration";
 import ScreenProfile from "../screens/Profile";
+import ScreenDetailroom from "../screens/Detailroom"
 
 import { NativeRouter, Route, Link } from "react-router-native";
 
@@ -71,50 +72,50 @@ function ProfileScreen() {
 export default function App() {
     return (
         <NativeRouter>
-        <>
-            <View
-                style={{
-                    flex: 1,
-                    flexGrow: 1,
-                    backgroundColor: "#FFDA79",
-                }}
-            >
-                <>
-                    <View
-                        style={{
-                            flex: 0,
-                            alignItems: 'center',
-                            height: 10,
-                            padding: 10,
-                            flexDirection: "row",
-                            backgroundColor: "white",
-                            borderRadius: 20,
-                            justifyContent: "space-between",
-                            marginHorizontal: 20,
-                            paddingVertical: 20,
-                            marginTop: 40,
-                            zIndex: 2
-                        }}
-                    >
-                        <View style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                        }}>
-                            <Image source={imgDooHor} style={{ resizeMode: "contain", width: "15%", bottom: 10, }} />
+            <>
+                <View
+                    style={{
+                        flex: 1,
+                        flexGrow: 1,
+                        backgroundColor: "#FFDA79",
+                    }}
+                >
+                    <>
+                        <View
+                            style={{
+                                flex: 0,
+                                alignItems: 'center',
+                                height: 10,
+                                padding: 10,
+                                flexDirection: "row",
+                                backgroundColor: "white",
+                                borderRadius: 20,
+                                justifyContent: "space-between",
+                                marginHorizontal: 20,
+                                paddingVertical: 20,
+                                marginTop: 40,
+                                zIndex: 2
+                            }}
+                        >
+                            <View style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                            }}>
+                                <Image source={imgDooHor} style={{ resizeMode: "contain", width: "15%", bottom: 10, }} />
+                            </View>
+                            <View style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                            }}>
+                                <Ionicons name="chatbubble-outline" size={24} color="black" style={{ position: "absolute", left: -60, top: -15 }} />
+                                <FontAwesome name="user" size={25} color={"black"} style={{ position: "absolute", left: -25, top: -15 }} />
+                            </View>
                         </View>
-                        <View style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                        }}>
-                            <Ionicons name="chatbubble-outline" size={24} color="black" style={{ position: "absolute", left: -60, top: -15 }} />
-                            <FontAwesome name="user" size={25} color={"black"} style={{ position: "absolute", left: -25, top: -15 }} />
-                        </View>
-                    </View>
-                    <ScreenProfile />
+                        <ScreenDetailroom />
 
-                </>
-            </View>
-        </>
+                    </>
+                </View>
+            </>
         </NativeRouter>
     );
 }
