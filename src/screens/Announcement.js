f2b2700ec6ed8f47d9f76699c44120bcc0b5a142
KITTIPOB PANGTRAKOON <63070010@kmitl.ac.th>
2022-11-19 22:10:01 +0700

    สองหน้า

--- src/screens/Announcement.js
+++ src/screens/Announcement.js
@@ -1,51 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import { StyleSheet, Text, View, Button, Image, Dimensions, Switch, SafeAreaView, ScrollView, TextInput, TouchableOpacity } from 'react-native';
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import firebase from '../Database/firebaseDB'
+import { useNavigation } from '@react-navigation/native';
 
-export default function Announcement() {
-    const [home, setHome] = useState([])
-    const all_data = []
-    // firebase.firestore()
-    //     .collection('declaration')
-    //     .where('state', '==', 'open')
-    //     .get()
-    //     .then(querySnapshot => {
-    //         querySnapshot.forEach((res) => {
-    //             const { desc, image, title, } = res.data();
-    //             all_data.push({ desc: desc, image: image, title: title, });
-    //         });
-    //         setHome(all_data);
+export default class Announcement extends Component {
+    constructor() {
+        super();
+        this.subjCollection = firebase.firestore().collection("office_documents");
+        this.state = {
+            subject_list: [],
+        };
+    }
+    getCollection = () => {
 
-    //     });
-    var data = [];
 
-    for (let i = 0; i < home.length; i++) {
-        data.push(
-            <View key={i}>
-                <View>
+        const all_data = [];
+        firebase.firestore()
+            .collection('declaration')
+            .where('state', '==', "open")
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach((res) => {
+                    const { desc, image, title, type, state } = res.data();
+                    all_data.push({ desc: desc, image: image, title: title, type: type, state: state, id: res.id });
+                });
+                setHome(all_data);
 
-                    <Image style={styles.img}
-                        source={{ uri: home[i].image }} />
-                    <View style={styles.description}>
-                        <Text style={{ padding: 10 }}>
-                            {home[i].desc}
-                        </Text>
+            });
+
+
+
+    };
+    componentDidMount() {
+        this.unsubscribe =
+            this.subjCollection.onSnapshot(this.getCollection);
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    ScreenRegister() {
+        const navigation = useNavigation();
+        navigation.navigate('ScreenRegister')
+    };
+    GeneralTopic(id) {
+        const navigation = useNavigation();
+        navigation.navigate('GeneralTopic', {
+            id,
+        })
+    };
+    ScreenHor() {
+        const navigation = useNavigation();
+        navigation.navigate('ScreenHor')
+    };
+    render() {
+        return (
+
+            < ScrollView style={styles.container} >
+                {this.state.subject_list.map((item, i) => (
+                    <View key={i}>
+                        <View style={{ display: item.type == 'Register' ? 'flex' : 'none' }}>
+                            <Image style={styles.img} source={{ uri: item.image }} />
+                            <TouchableOpacity onPress={() => ScreenRegister()}>
+                                <SafeAreaView style={styles.description}>
+                                    <Text style={{ padding: 10 }}>
+                                        {item.title} : {item.desc}   ...กดเพื่อดูรายละเอียดเพิ่มเติม
+                                    </Text>
+                                </SafeAreaView>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{ display: item.type == 'Room' ? 'flex' : 'none' }}>
+                            <Image style={styles.img} source={{ uri: item.image }} />
+                            <TouchableOpacity onPress={() => ScreenHor()}>
+                                <SafeAreaView style={styles.description}>
+                                    <Text style={{ padding: 10 }}>
+                                        {item.title} : {item.desc}   ...กดเพื่อดูรายละเอียดเพิ่มเติม
+                                    </Text>
+                                </SafeAreaView>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={{ display: item.type == 'General' ? 'flex' : 'none' }}>
+                            <Image style={styles.img} source={{ uri: item.image }} />
+                            <TouchableOpacity onPress={() => GeneralTopic(item.id)}>
+                                <SafeAreaView style={styles.description}>
+                                    <Text style={{ padding: 10 }}>
+                                        {item.title} : {item.desc}   ...กดเพื่อดูรายละเอียดเพิ่มเติม
+                                    </Text>
+                                </SafeAreaView>
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
-                </View>
 
-            </View>
-        )
+                ))}
+
+            </ScrollView>
+
+
+        );
     }
-    return (
 
-        < ScrollView style={styles.container} >
-            {data}
-        </ScrollView>
-
-
-    );
 }
 
 const styles = StyleSheet.create({
@@ -66,10 +120,12 @@ const styles = StyleSheet.create({
         width: "80%",
         height: "100%",
         alignSelf: "center",
-        marginTop: 15,
+        marginTop: 10,
         flex: 1,
         marginBottom: 30,
-        minHeight: 200
+        borderRadius: 15,
+        elevation: 15,
+        padding: 10,
     }
 
 });
---
+++ src/screens/GeneralTopic.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, Component } from 'react';
+import { StyleSheet, Text, View, Button, Image, Dimensions, Switch, SafeAreaView, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import firebase from '../Database/firebaseDB'
+import { useRoute } from '@react-navigation/native';
+
+export default class Announcement extends Component {
+    constructor() {
+        super();
+        this.subjCollection = firebase.firestore().collection("office_documents");
+        this.state = {
+            subject_list: [],
+        };
+    }
+    getCollection = () => {
+        const route = useRoute();
+        const user = firebase.auth().currentUser
+        if (user) {
+            const all_data = [];
+            firebase.firestore()
+                .collection('declaration')
+                .where('id', '==', route.params.id)
+                .get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach((res) => {
+                        const { desc, image, title, type, state } = res.data();
+                        all_data.push({ desc: desc, image: image, title: title, type: type, state: state, id: res.id });
+                    });
+                    setHome(all_data);
+
+                });
+
+
+        }
+        else {
+
+        }
+    };
+    componentDidMount() {
+        this.unsubscribe =
+            this.subjCollection.onSnapshot(this.getCollection);
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+
+        return (
+
+            < ScrollView style={styles.container} >
+                {this.state.subject_list.map((item, i) => (
+                    <View key={i}>
+
+                        <View>
+                            <Image style={styles.img} source={{ uri: item.image }} />
+                            <TouchableOpacity>
+                                <SafeAreaView style={styles.description}>
+                                    <Text style={{ padding: 10 }}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={{ padding: 10 }}>
+                                        {item.desc}
+                                    </Text>
+
+                                </SafeAreaView>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                ))}
+
+            </ScrollView>
+
+
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFDA79",
+    },
+    img: {
+        width: "80%",
+        height: 150,
+        resizeMode: "cover",
+        borderRadius: 10,
+        alignSelf: "center",
+    },
+    description: {
+        backgroundColor: "white",
+        borderRadius: 20,
+        width: "80%",
+        height: "100%",
+        alignSelf: "center",
+        marginTop: 10,
+        flex: 1,
+        marginBottom: 30,
+        borderRadius: 15,
+        elevation: 15,
+        padding: 10,
+    }
+
+});
+
